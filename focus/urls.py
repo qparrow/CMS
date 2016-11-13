418c267cf32f9bb6a5b68a41d74a5af9d2e8b468
qparrow <1433108478@qq.com>
2016-11-13 18:32:39 +0800

    CMS

--- focus/urls.py
+++ focus/urls.py
@@ -6,8 +6,8 @@ urlpatterns=[
 	#url(r'^register/$',views.register,name='register'),
 	url(r'^login/$',views.log_in,name='login'),
 	url(r'^logout/$',views.log_out,name='logout'),
-	#url(r'^(?p<article_id>[0-9]+)/comment/$',views.comment,name='comment'),
-	#url(r'^(?p<article_id>[0-9]+)/keep/$',views.get_keep,name='keep'),
-	#url(r'^(?p<article_id>[0-9]+)/poll/$',views.get_poll_article,name='poll'),
-	#url(r'^(?p<article_id>[0-9]+)//$',views.article,name='article'),
+	url(r'^(?P<article_id>[0-9]+)/comment/$',views.comment,name='comment'),
+	url(r'^(?P<article_id>[0-9]+)/keep/$',views.get_keep,name='keep'),
+	url(r'^(?P<article_id>[0-9]+)/poll/$',views.get_poll_article,name='poll'),
+	url(r'^(?P<article_id>[0-9]+)//$',views.article,name='article'),
 ]
--- focus/views.py
+++ focus/views.py
@@ -37,10 +37,111 @@ def log_in(request):
 			return render(request,'login.html',{'form':form})
 
 ###############################################################################
+
 @login_required
 def log_out(request):
 	url=request.POST.get('source_url','/focus/')
 	logout(request)
 	return redirect(url)
 
+###############################################################################
+
+def article(request,article_id):
+	article=get_object_or_404(Article,id=article_id)
+	content=markdown2.markdown(article.content,extras=['code-friendly','fenced-code-blocks','header-ids','toc','metadata'])
+	commentform=CommentForm()
+	loginform=LoginForm()
+	comments=article.comment_set.all
+	return render(request,'article_page.html',{'article':article,'loginform':loginform,
+		'commentform':comment,
+		'content':content,
+		'comment':omments
+		})
+
+###############################################################################
+
+@login_required
+def comment(request,article_id):
+	form=CommentForm(request.POST)
+	url=urlparse.urljoin('/focus/',article_id)
+	if form.is_valid():
+		user=request.user
+		article=Article.objects.get(id=aritcle_id)
+		new_comment=form.cleaned_data['comment']
+		c=Comment(content=new_comment,article_id=article_id)
+		c.user=user
+		c.save()
+		article.comment_num+=1
+		return direct(url)
+
+###############################################################################
+
+@login_required
+def get_keep(request,article_id):
+	logged_user=request.user
+	article=Article.object.get(id=article_id)
+	articles=logged_user.article_set.all()
+	if article not in articles:
+		article.user.add(logged_user)
+		article.keep_num+=1
+		article.save()
+		return redirect('/focus/')
+	else:
+		url=urlparse.urljoin('focus/',article_id)
+		return redirect(url)
+
+###############################################################################
+
+@login_required
+def get_poll_article(request,article_id):
+	logged_user=request.user
+	article=Article.object.get(id=article_id)
+	polls=logged_user.poll_set.all()
+	articles=[]
+	for poll in polls:
+		articles.append(poll.article)
+	if article in articles:
+		url=urlparse.urljoin('/focus/',article_id)
+		return redirect(url)
+	else:
+		article.poll_num+=1
+		article.save()
+		poll=Poll(user=logged_user,article=article)
+		poll.save()
+		data=[]
+		return redirect('/focus/')
+
+###############################################################################
+'''
+def register(request):
+	error1="this name already exist"
+	valid='this name is valid'
+
+	if reuqies.method=='GET':
+		form=RegisterForm()
+		return render(request,'register.html',{'form':form})
+	if request.method=='POST':
+		form=RegisterForm(request.POST)
+		if request.POST.get('raw_username','erjgiqfv240hqp5668ej23foi')!='erjgiqfv240hqp5668ej23foi':
+			try:
+				user=NewUser.objects.get(username=request.POST.get('raw_username',''))
+			except:ObjectDoesNotExist:
+				return render(request,'register.html',{'form':form,'msg':valid})
+			else:
+				return render(request,'register.html',{'form':form,'msg':error1})
+		else:
+			if form.is_valid():
+				username=form.cleaned_data['username']
+				email=form.cleaned_data['email']
+				passwod1=form.cleaned_data['password1']
+				password2=form.cleaned_data['password2']
+				if password1!=password2:
+					return render(request,'register.html',{'form':form,'msg':'two password is not equal'})
+				else:
+					user=NewUser(username=username,email=email,password=password1)
+					user.save()
+					return redirect('/focus/login')
+			else:
+				return render(request,'register.html',{'form':form})
+'''
 
